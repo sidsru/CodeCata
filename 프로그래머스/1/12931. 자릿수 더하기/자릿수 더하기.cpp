b3afa7a4351cb74c77adacc0360f0df7319c10ad
sidsru <sidsru21131210@gmail.com>
2025-08-20 09:19:25 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    while (n>=1)
+    {
+        answer += n%10;
+        n = n/10;
+    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    //cout << "Hello Cpp" << endl;
+
+    return answer;
+}
