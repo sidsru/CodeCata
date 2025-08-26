8b4c6075cc583b751d4bb0334dbe01313e8f24a2
sidsru <sidsru21131210@gmail.com>
2025-08-26 09:44:44 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer ="  ";
+    if(s.length()%2 == 0)
+    {
+        answer[0] = s[s.length()/2 -1];
+        answer[1] = s[s.length()/2];
+    }
+    else
+    {
+        answer = s[s.length()/2];
+    }
+    return answer;
+}
