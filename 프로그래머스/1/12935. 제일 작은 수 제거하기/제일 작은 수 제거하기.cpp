ffa012737040cb138b5e423a15f4fa4f50543c21
sidsru <sidsru21131210@gmail.com>
2025-08-26 09:35:46 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 10.77 ms, Memory: 13.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    if(arr.size() == 1) 
+    {
+        arr.pop_back();
+        arr.push_back(-1);
+        return arr;
+    }
+    arr.erase(min_element(arr.begin(), arr.end()));
+    
+    return arr;
+}
