e7a53f8218a13c59ebe695630482109d9fe4860d
sidsru <sidsru21131210@gmail.com>
2025-09-02 22:46:10 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num) {
+    vector<int> answer;
+    int odd = 0;
+    int even = 0;
+    for(int i=0; i<num.size();i++)
+    {
+        if(num[i]%2 == 0)
+        {
+            ++even;
+        }
+        else
+        {
+            ++odd;
+        }
+    }
+    answer.push_back(even);
+    answer.push_back(odd);
+    return answer;
+}
