d44b7c766e954a530f1fec97d61d193d0851774c
sidsru <sidsru21131210@gmail.com>
2025-09-03 00:52:11 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cpp
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cpp
@@ -5,9 +5,5 @@ using namespace std;
 
 int solution(vector<int> box, int n) {
     int answer = 0;
-    int a = box[0] * box[1];
-    int b = n*n;
-    int c = a / b;
-    int d = box[2]/n;
-    return answer = c * d;
+    return answer = (box[0]/n) * (box[1]/n) * (box[2]/n);
 }
