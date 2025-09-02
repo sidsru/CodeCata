30f2bd0463a3b5614dee99904c2d19b2dada2594
sidsru <sidsru21131210@gmail.com>
2025-09-03 00:31:07 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = 0;
+    if(price >= 500000)
+    {
+        price *= 0.8;
+    }
+    else if(price >= 300000)
+    {
+        price *= 0.9;
+    }
+    else if(price >= 100000)
+    {
+        price *= 0.95;
+    }
+    return answer = price;
+}
