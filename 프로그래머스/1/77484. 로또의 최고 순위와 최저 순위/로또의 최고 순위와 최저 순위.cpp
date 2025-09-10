c3a1e5480ca5635ea3d1c428e16ec2d18a085ea0
sidsru <sidsru21131210@gmail.com>
2025-09-10 09:18:14 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> lottos, vector<int> win) {
+    vector<int> answer;
+    int temp = 0;
+    int dang = 0;
+    int arr[7] = {6 ,6, 5, 4, 3, 2, 1};
+    // 로또 알아볼수있는 부분만 맞췄다면 최저등수
+    // 로또의 알아볼수 없는 부분도 모두 맞췄다면 최고 등수
+    
+    // 민우의 로또 번호 순회
+    for ( auto a : lottos)
+    {
+        //해당 번호가 있는지 확인
+        if (a == 0) 
+        {
+            ++temp;
+        }
+        else if (find(win.begin(), win.end(), a) != win.end())
+        {
+            ++dang;
+        }
+    }
+    answer.push_back(arr[dang+temp]);
+    answer.push_back(arr[dang]);
+    return answer;
+}
