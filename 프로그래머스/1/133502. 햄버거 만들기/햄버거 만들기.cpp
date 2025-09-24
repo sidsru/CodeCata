6aca5c4892c4917f4d8f92111ef5b9b2b7a6a751
sidsru <sidsru21131210@gmail.com>
2025-09-24 09:59:52 +0900

    [level 1] Title: 햄버거 만들기, Time: 68.64 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -1,25 +1,26 @@
 #include <string>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+bool check4(vector<int>& v){
+    if(vector<int>(v.end()-4, v.end()) == vector<int>{1,2,3,1}){
+        return true;
+    }
+    return false;
+}
+
 int solution(vector<int> ingredient) {
     int answer = 0;
-    int temp[1000000];
-    int i = 0;
-    // 1, 2, 3, 1 순서대로 배열이 나열되어있다면 asnwer를 하나 올려주고 배열을 제거
-    // 위 과정을 더 이상 진행할수 없을 때 까지 반복한다
-    for(int& a : ingredient)
-    {
-        temp[i] = a;
-        if(i >= 3)
-            if(temp[i] == 1 && temp[i-1] == 3 && temp[i-2] == 2 && temp[i-3] == 1)
-            {
-                ++answer;
-                i -= 4;
-            }
-        ++i;
+    vector<int> v;
+    for(int it: ingredient){
+        v.push_back(it);
+        while(check4(v)){
+            for(int i=0; i<4; i++) v.pop_back();
+            answer++;
+        }
     }
-    
     return answer;
 }
