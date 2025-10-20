e119791c829c61f2d8bbf38f4bc4904e134b195a
sidsru <sidsru21131210@gmail.com>
2025-10-20 10:11:45 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,14 @@
+/*MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        sum <- sum + A[i]; # 코드1
+    return sum;
+}*/
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::cout << n << std::endl << 1; 
+}
