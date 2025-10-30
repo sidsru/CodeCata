20e6d5b72523221074da4b46d7767baabe15df09
sidsru <sidsru21131210@gmail.com>
2025-10-30 12:25:30 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef struct 
+{
+    int x;
+    int y;
+}coordinate;
+
+bool comp(coordinate a, coordinate b)
+{
+    if(a.x == b.x) return a.y < b.y;
+    return a.x < b.x;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, X, Y;
+    cin >> n;
+    vector<coordinate> dot;
+    for(int i=0; i<n; i++)
+    {
+        cin >> X >> Y;
+        coordinate temp;
+        temp.x = X, temp.y = Y;
+        dot.push_back(temp);
+    }
+    sort(dot.begin(), dot.end(), comp);
+    for(coordinate& a : dot)
+    {
+        cout << a.x << " " << a.y << "\n";
+    }
+    return 0;
+}
