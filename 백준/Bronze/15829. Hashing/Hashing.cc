67535f25aad910efa9fbf2f439e210c6e980cd39
sidsru <sidsru21131210@gmail.com>
2025-11-02 22:53:16 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,26 +1,20 @@
 #include<iostream>
 using namespace std;
-long long square(long long n, int i)
-{
-    for(int j=0; j<i; j++)
-    {
-        n *= 31;
-        n %= 1234567891;
-    }
-    return n;
-}
+
 int main ()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    long long l,sum = 0;
+    int l;
+    long long r = 1,sum = 0, m = 1234567891;
     cin >> l;
     string s;
     cin >> s;
     for(int i=0; i< l; i++)
     {
-        sum += square((s[i]-'a')+1, i);
+        sum += ((int)(s[i]-'a'+1) * r) % m;
+        r = (r*31) % m;
     }
-    cout << sum;
+    cout << sum % m;
 }
