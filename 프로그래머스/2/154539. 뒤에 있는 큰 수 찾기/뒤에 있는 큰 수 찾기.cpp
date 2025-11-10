2aeea75354ae5315ab7cefa4556324840d1d310d
sidsru <sidsru21131210@gmail.com>
2025-11-10 09:51:39 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 432.59 ms, Memory: 240 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer(numbers.size(), -1);
+    stack<int> s;
+    s.push(0);
+    for(int i=0; i< numbers.size(); i++)
+    {
+        while (!s.empty() && numbers[s.top()] < numbers[i])
+        {
+            answer[s.top()] = numbers[i];
+            s.pop();
+        }
+        s.push(i);
+    }
+    return answer;
+}
