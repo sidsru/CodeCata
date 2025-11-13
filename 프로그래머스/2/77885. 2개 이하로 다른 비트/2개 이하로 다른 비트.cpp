2af4cf51e3a32a72fcfcc013a54e9fe69143366a
sidsru <sidsru21131210@gmail.com>
2025-11-13 10:18:58 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 36.95 ms, Memory: 25 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+    for(int i=0; i<numbers.size(); ++i)
+    {
+        if(numbers[i]%2 == 0)
+            answer.push_back(numbers[i]+1);
+        else
+        {
+            long long bit = 1;
+            while(1)
+            {
+                if(!(numbers[i] & bit)) break;
+                bit *= 2;
+            }
+            answer.push_back(numbers[i] + (bit/2));
+        }
+    }
+    return answer;
+}
