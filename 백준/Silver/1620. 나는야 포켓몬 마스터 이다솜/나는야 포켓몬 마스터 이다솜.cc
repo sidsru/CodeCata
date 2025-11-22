81f0df56db351a9bda69d4827fd45ac3b0c867b5
sidsru <sidsru21131210@gmail.com>
2025-11-23 01:01:29 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 136 ms, Memory: 20860 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,32 +1,35 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <cctype>
 using namespace std;
+string Name[100001];
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    map<string, int> pokemon;
     int n,m;
     cin >> n >> m;
-    map<string, string> pokemonNumber;
-    map<string, string> pokemonName;
     for(int i=1; i<=n; i++)
     {
-        string s;
-        cin >> s;
-        pokemonNumber[to_string(i)] = {s};
-        pokemonName[s] = {to_string(i)};
+        cin >> Name[i];
+        pokemon.insert(make_pair(Name[i], i));
     }
     for(int i=0; i<m; i++)
     {
         string s;
         cin >> s;
-        if(pokemonNumber.find(s) != pokemonNumber.end())
-            cout << pokemonNumber[s] << '\n';
-        if(pokemonName.find(s) != pokemonName.end())
-            cout << pokemonName[s] << '\n';
+        if(isdigit(s[0]))
+            cout << Name[stoi(s)] << '\n';
+        if(!isdigit(s[0]))
+        {
+            map<string, int>::iterator it = pokemon.find(s);
+            cout << it->second << '\n';
+        }
+        
     }
     return 0;
 }
