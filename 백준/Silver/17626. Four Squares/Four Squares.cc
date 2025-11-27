5e73832b9bfe0d3b125d92b2e50fe6d7b1deca88
sidsru <sidsru21131210@gmail.com>
2025-11-27 10:45:21 +0900

    [Silver III] Title: Four Squares, Time: 16 ms, Memory: 2096 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+inline int Min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int dp[50001];
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++)
+    {
+        dp[i] = dp[i-1] + 1;
+        for(int j=2; j*j<=i; j++)
+        {
+            dp[i] = min(dp[i], dp[i - j*j] + 1);
+        }
+    }
+    cout << dp[n] << '\n';
+}
