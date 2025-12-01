432063659a7b29a88d39484120e1ebe3157a54cd
sidsru <sidsru21131210@gmail.com>
2025-12-01 10:39:38 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -6,16 +6,12 @@ using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
-    unordered_map<string, int> clothesmap;
-    for(int i=0; i<clothes.size(); i++)
-    {
-        ++clothesmap[clothes[i][1]];
-    }
-    for(unordered_map<string, int>::iterator it = clothesmap.begin(); it != clothesmap.end(); it++)
-    {
-        //cout << it->first << " " << it->second << endl;
-        answer *= it->second + 1;
-    }
+    unordered_map<string, int> um;
+    for(vector<string> clothe : clothes)
+        ++um[clothe[1]];
+    
+    for(unordered_map<string, int>::iterator it = um.begin(); it != um.end(); ++it)
+        answer *= it->second + 1; 
     
     return answer - 1;
 }
