32056486af47e1df949aa0feec0ee069b690a4d7
sidsru <sidsru21131210@gmail.com>
2025-12-03 11:37:45 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 112 ms, Memory: 5608 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<vector>
+#include <queue>
+using namespace std;
+vector<int> v[1001];
+bool bis[1001] = {false, };
+int answer = 0;
+
+void DFS(int n)
+{
+    if(bis[n])
+        return;
+    queue<int> q;
+    q.push(n);
+    while(!q.empty())
+    {
+        int temp = q.front();
+        q.pop();
+        for(int i=0; i<v[temp].size(); ++i)
+        {
+            if(!bis[v[temp][i]])
+            {
+                bis[v[temp][i]] = true;
+                q.push(v[temp][i]);
+            }
+        }
+    }
+    ++answer;
+    return;
+}
+int main()
+{
+    int N, M;
+    scanf("%d %d", &N, &M);
+    for(int i=0; i<M; ++i)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        v[x].push_back(y);
+        v[y].push_back(x);
+    }
+    for(int i=1; i<=N; ++i)
+    {
+        DFS(i);
+    }
+    printf("%d", answer);
+    return 0;
+}
