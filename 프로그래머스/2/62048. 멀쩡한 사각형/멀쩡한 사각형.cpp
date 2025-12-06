d3534ccb42071a97e1f4929d3a6b647d43d77ec3
sidsru <sidsru21131210@gmail.com>
2025-12-06 22:45:33 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 112.15 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+long long solution(int w,int h) {
+    long long answer = 0;
+    for(int i=1; i<w; ++i)
+    {
+        long long n = ((double)h * i )/w;
+        answer += n;
+    }
+    answer *= 2;
+    return answer;
+}
