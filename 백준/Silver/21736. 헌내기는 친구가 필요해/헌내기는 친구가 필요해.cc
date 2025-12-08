a9b585d8f7f9a762703fe6deb81d10d193ea285c
sidsru <sidsru21131210@gmail.com>
2025-12-08 12:40:32 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 20 ms, Memory: 13072 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+int xmove[4] = {0, 0, 1, -1};
+int ymove[4] = {1, -1, 0, 0};
+bool vis[601][601] = {false, };
+string s[601];
+int answer = 0, n ,m;
+
+void BFS(const int x, const int y)
+{
+    vis[x][y] = true;
+    if(s[x][y] =='P')
+        ++answer;
+    for(int i=0; i<4; i++)
+    {
+        int xx = x + xmove[i];
+        int yy = y + ymove[i];
+        if(xx >= n || yy >= m || xx < 0 || yy < 0)
+            continue;
+        if(vis[xx][yy] || s[xx][yy] == 'X')
+            continue;
+        BFS(xx, yy);
+    }
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int ix = -1, iy = -1;
+    cin >> n >> m;
+    for(int i=0; i<n; ++i)
+    {
+        cin >> s[i];
+        if(ix == -1 && iy == -1)
+            for(int j=0; j<s[i].size(); ++j)
+            {
+                if(s[i][j] == 'I')
+                {
+                    ix = i;
+                    iy = j;
+                }
+            }
+    }
+    BFS(ix, iy);
+    if(!answer)
+    {
+        cout << "TT";
+        return 0;
+    }
+    cout << answer;
+    return 0;
+}
