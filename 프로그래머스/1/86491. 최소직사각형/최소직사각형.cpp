721e0aebf374aada7f86bd928f1f03afbaad18a4
sidsru <sidsru21131210@gmail.com>
2025-12-10 10:11:00 +0900

    [level 1] Title: 최소직사각형, Time: 0.92 ms, Memory: 4.92 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -1,26 +1,24 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
+
+inline int Max(const int& a, const int& b){return  a > b ? a : b;}
+
 int solution(vector<vector<int>> sizes) {
-    int answer = 0;
     int h = 0;
     int w = 0;
-    for(int i=0; i<sizes.size(); i++)
+    for(vector<int> v : sizes)
     {
-        if(sizes[i][0] > sizes[i][1])
+        if(v[0] > v[1])
         {
-            w = max(w, sizes[i][0]);
-            h = max(h, sizes[i][1]);
+            h = Max(h, v[0]);
+            w = Max(w, v[1]);
         }
         else
         {
-            w = max(w, sizes[i][1]);
-            h = max(h, sizes[i][0]);
+            h = Max(h, v[1]);
+            w = Max(w, v[0]);
         }
     }
-    // 제일 큰 w와 h의 곱을 지갑크기로 출력
-    answer = h * w;
-    return answer;
+    return h * w;
 }
