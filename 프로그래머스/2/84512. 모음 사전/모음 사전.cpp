4f2ec0ba08550b83a07a0bc1aa5a6491115cda90
sidsru <sidsru21131210@gmail.com>
2025-12-10 10:28:09 +0900

    [level 2] Title: 모음 사전, Time: 0.12 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -8,17 +8,16 @@ string target = "";
 string aeoiu = "AEIOU";
 void dfs(string s)
 {
-    ++cnt;;
-    if(target == s)
+    ++cnt;
+    if(s == target)
     {
         answer = cnt;
         return;
     }
-    if(s.size() >= aeoiu.size())return;
-    for(int i=0; i<aeoiu.size(); i++)
-    {
+    if(s.size() >= 5)
+        return;
+    for(int i=0; i<5; ++i)
         dfs(s + aeoiu[i]);
-    }
 }
 int solution(string word) {
     target = word;
