128ecbf3d457b59744811024b22c6f4043ef2aa4
sidsru <sidsru21131210@gmail.com>
2025-12-17 09:53:14 +0900

    [level 2] Title: 디펜스 게임, Time: 37.86 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+int solution(int n, int k, vector<int> enemy) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(int i=0; i<enemy.size(); ++i)
+    {
+        pq.push(enemy[i]);
+        if(pq.size()>k)
+        {
+            n -= pq.top();
+            pq.pop();
+        }
+        if(n<0)
+            return i;
+    }
+    return enemy.size();
+}
