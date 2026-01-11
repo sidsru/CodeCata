e5361965643d0a00a02fb248d54b5a902b167d79
sidsru <sidsru21131210@gmail.com>
2026-01-11 22:37:42 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 164 ms, Memory: 5216 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -12,18 +12,14 @@ int main()
 		for (int j = 1; j <= n; ++j)
 		{
 			scanf("%d", &map[i][j]);
-			map[i][j] += map[i][j - 1];
+			map[i][j] += map[i][j - 1] + map[i-1][j] - map[i-1][j-1];
 		}
 	}
 	for (int i = 0; i < m; ++i)
 	{
 		int x1, y1, x2, y2;
 		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-		int sum = 0;
-		for (int x = x1; x <= x2; ++x)
-		{
-			sum += map[x][y2] - map[x][y1 - 1];
-		}
+		int sum = map[x2][y2] - map[x2][y1-1] - map[x1-1][y2] + map[x1-1][y1-1];
 		printf("%d\n", sum);
 	}
 	return 0;
