77c8045e1ba69b111c6f1521fca3821159952a12
sidsru <sidsru21131210@gmail.com>
2026-02-01 22:27:59 +0900

    [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,67 +1,44 @@
-// 백준
 #include <iostream>
-#define endl '\n'
+#include <algorithm>
 using namespace std;
-constexpr int INF = 1e9;//(~0U >> 1);
 
-int n, m;
-int arr[101][101];
-inline void Input()
-{
+const int INF = 1e9;
+int dist[101][101];
 
-}
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	//Input();
-	cin >> n;
-	cin >> m;
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			if (i == j)
-			{
-				arr[i][i] = 0;
-			}
-			else
-			{
-				arr[i][j] = INF;
-			}
-		}
-	}
-	for (int i = 0; i < m; i++)
-	{
-		int from, to, cost;
-		cin >> from >> to >> cost;
-		arr[from][to] = min(cost, arr[from][to]);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	}
-	for (int mid = 1; mid <= n; mid++)
-	{
-		for (int from = 1; from <= n; from++)
-		{
-			for (int to = 1; to <= n; to++)
-			{
-				arr[from][to] = min(arr[from][mid] + arr[mid][to], arr[from][to]);
-			}
-		}
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			if (arr[i][j] == INF)
-			{
-				cout << 0 << ' ';
-			}
-			else
-			{
-				cout << arr[i][j] << ' ';
-			}
-		}
-		cout << endl;
-	}
-	return 0;
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == j) dist[i][j] = 0;
+            else dist[i][j] = INF;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        dist[a][b] = min(dist[a][b], c);
+    }
+
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (dist[i][j] == INF) cout << 0 << " ";
+            else cout << dist[i][j] << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
 }
