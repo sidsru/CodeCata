b9c14f26c71c68e0859a6ffa97c59746b3826e95
sidsru <sidsru21131210@gmail.com>
2026-02-08 21:36:07 +0900

    [Silver IV] Title: 대칭 차집합, Time: 60 ms, Memory: 3592 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,29 +1,57 @@
 // 백준
 #include <iostream>
-#include <unordered_map>
+#include <vector>
+#include <algorithm>
 #define endl '\n'
 using namespace std;
 constexpr int INF = (~0U >> 1);
+bool BinarySearch(const vector<int>& v, const int target)
+{
+	int start = 0;
+	int end = v.size() - 1;
+	while (start <= end)
+	{
+		int mid = (start + end) / 2;
+		if (v[mid] == target)
+		{
+			return true;
+		}
+
+		if (v[mid] < target)
+		{
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid - 1;
+		}
+	}
+	return false;
+}
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	unordered_map<int, int> um;
 	int n, m;
 	cin >> n >> m;
-	for (int i = 0; i < n+m; i++)
+	vector<int> a(n), b(m);
+	for (int i = 0; i < n; i++)
 	{
-		int temp;
-		cin >> temp;
-		um[temp]++;
-
+		cin >> a[i];
 	}
-	int answer = 0;
-	for (unordered_map<int, int>::iterator it = um.begin(); it != um.end(); it++)
+	sort(a.begin(), a.end());
+	for (int i = 0; i < m; i++)
 	{
-		if (it->second & 1)
+		cin >> b[i];
+	}
+	sort(b.begin(), b.end());
+	int answer = a.size()+ b.size();
+	for (int temp : a)
+	{
+		if (BinarySearch(b, temp))
 		{
-			answer++;
+			--answer;
+			--answer;
 		}
 	}
 	cout << answer;
