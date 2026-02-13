c8f9a01b3bc1274158c9e16dd0d8992b42a1d93e
sidsru <sidsru21131210@gmail.com>
2026-02-13 10:35:38 +0900

    [Gold III] Title: 파티, Time: 4 ms, Memory: 6084 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,53 +1,94 @@
 // 백준
 #include <iostream>
+#include <queue>
 #define endl '\n'
 using namespace std;
 constexpr int INF = (~0U >> 1);
-int answer = 0;
+int n, m, x;
+int Distance[1001];
+int TotalDistance[1001];
 int map[1001][1001];
+void dijkstra(const int start)
+{
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	for (int i = 1; i <= n; i++)
+	{
+		Distance[i] = INF;
+	}
+	Distance[start] = 0;
+	pq.push({ 0, start });
+	while (!pq.empty())
+	{
+		int Dis = pq.top().first;
+		int value = pq.top().second;
+		pq.pop();
+		for (int i = 1; i <= n; i++)
+		{
+			if (map[value][i] == 0)
+				continue;
+			if (Dis + map[value][i] < Distance[i])
+			{
+				Distance[i] = Dis + map[value][i];
+				pq.push({ Distance[i],i });
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		TotalDistance[i] += Distance[i];
+	}
+}
+void dijkstraRevers(const int start)
+{
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	for (int i = 1; i <= n; i++)
+	{
+		Distance[i] = INF;
+	}
+	Distance[start] = 0;
+	pq.push({ 0, start });
+	while (!pq.empty())
+	{
+		int Dis = pq.top().first;
+		int value = pq.top().second;
+		pq.pop();
+		if (Dis > Distance[value])
+			continue;
+		for (int i = 1; i <= n; i++)
+		{
+			if (map[i][value] == 0)
+				continue;
+			if (Dis + map[i][value] < Distance[i])
+			{
+				Distance[i] = Dis + map[i][value];
+				pq.push({ Distance[i], i });
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		TotalDistance[i] += Distance[i];
+	}
+}
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	int n, m, x;
+	cout.tie(nullptr);
 	cin >> n >> m >> x;
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			if ((i == j))
-			{
-				map[i][j] = 0;
-				continue;
-			}
-			map[i][j] = INF;
-		}
-	}
 	for (int i = 0; i < m; i++)
 	{
 		int from, to, cost;
 		cin >> from >> to >> cost;
 		map[from][to] = cost;
 	}
-	for (int k = 1; k <= n; k++)
-	{
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = 1; j <= n; j++)
-			{
-				if (i == j)
-					continue;
-				if (map[i][k] == INF || map[k][j] == INF)
-				{
-					continue;
-				}
-				map[i][j] = min(map[i][j], map[i][k] + map[k][j]);
-			}
-		}
-	}
+	dijkstra(x);
+	dijkstraRevers(x);
+
+	int answer = 0;
 	for (int i = 1; i <= n; i++)
 	{
-		answer = max(answer, map[i][x] + map[x][i]);
+		answer = max(answer, TotalDistance[i]);
 	}
 	cout << answer;
 	return 0;
