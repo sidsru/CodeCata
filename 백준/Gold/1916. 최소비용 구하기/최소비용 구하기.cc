c0e0229fc3177881c1b78f31cfd7fbbb165cd305
sidsru <sidsru21131210@gmail.com>
2026-02-18 22:23:06 +0900

    [Gold V] Title: 최소비용 구하기, Time: 16 ms, Memory: 3716 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -1,68 +1,59 @@
 // 백준
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include <iostream>
+#include <cstring>
 #include <queue>
-#define INF ~0U>>1
+#define endl '\n'
 using namespace std;
-
+constexpr int INF = (~0U >> 1);
 int n, m;
-int totalcost[1001];
-int bus[1001][1001];
-
+int map[1001];
+vector<pair<int, int>> v[1001];
 void Dijkstra(const int start, const int end)
 {
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	totalcost[start] = 0;
-	pq.push({ totalcost[start] ,start });
-	while (!pq.empty())
+	for (int i = 1; i <= n; i++)
 	{
-		int cost = pq.top().first;
-		int node = pq.top().second;
-		pq.pop();
-		for (int nextnode = 1; nextnode <= n; ++nextnode)
+		map[i] = INF;
+	}
+	queue<pair<int, int>> q;
+	q.push(make_pair(start, 0));
+	map[start] = 0;
+	while (q.empty() == false)
+	{
+		int now = q.front().first;
+		int cost = q.front().second;
+		q.pop();
+		if (cost > map[now])
 		{
-			if (bus[node][nextnode] == INF)
+			continue;
+		}
+		for (int i = 0; i < v[now].size(); i++)
+		{
+			int next = v[now][i].first;
+			int ncost = v[now][i].second;
+			if (ncost + cost < map[next])
 			{
-				continue;
-			}
-			if (cost + bus[node][nextnode] < totalcost[nextnode])
-			{
-				totalcost[nextnode] = cost + bus[node][nextnode];
-				pq.push({ totalcost[nextnode] , nextnode });
+				map[next] = ncost + cost;
+				q.push(make_pair(next, map[next]));
 			}
 		}
 	}
-	printf("%d", totalcost[end]);
+	cout << map[end];
 }
-
-void input()
-{
-	scanf("%d %d", &n, &m);
-	for (int i = 1; i <= n; ++i)
-	{
-		totalcost[i] = INF;
-		for (int j = 1; j <= n; ++j)
-		{
-			bus[i][j] = INF;
-		}
-	}
-	for (int i = 0; i < m; ++i)
-	{
-		int from, to, cost;
-		scanf("%d %d %d", &from, &to, &cost);
-		if (bus[from][to] > cost)
-		{
-			bus[from][to] = cost;
-		}
-	}
-	int start, end;
-	scanf("%d %d", &start, &end);
-	Dijkstra(start, end);
-}
-
 int main()
 {
-	input();
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin >> n >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int from, to, cost;
+		cin >> from >> to >> cost;
+		v[from].push_back(make_pair(to, cost));
+	}
 
+	int start, end;
+	cin >> start >> end;
+	Dijkstra(start, end);
 	return 0;
 }
