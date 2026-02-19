5f7c1d87cf5edf41f80828065c79f85013f2d19c
sidsru <sidsru21131210@gmail.com>
2026-02-19 09:58:09 +0900

    [Silver IV] Title: 다음 소수, Time: 412 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -0,0 +1,49 @@
+// 백준
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#define endl '\n'
+using namespace std;
+constexpr int INF = (~0U >> 1);
+
+bool Pirme(const long long n)
+{
+	if (n <= 1)
+		return false;
+	if (n == 2 || n == 3)
+		return true;
+	if (n % 2 == 0 || n % 3 == 0)
+		return false;
+	for (long long i = 5; i * i <= n; ++i)
+	{
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+	}
+	return true;
+}
+long long findPirme(long long start)
+{
+	while (true)
+	{
+
+		if (Pirme(start) == true)
+			return start;
+
+		++start;
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		long long temp;
+		cin >> temp;
+		cout << findPirme(temp) << endl;
+	}
+	return 0;
+}
